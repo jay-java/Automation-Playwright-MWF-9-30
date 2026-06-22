fdb9c077ecd3db431d448f93386a9718d98ccf27
jay-java <jayprajapati.tops@gmail.com>
2026-06-22 10:43:18 +0530

    python intro

---
+++ Python/P001_first.py
@@ -0,0 +1,8 @@
+#comment
+#print is pre-define function in python to print data
+print('hello python')
+print("hello python")
+contact = 987654321
+per = 85.32
+name = 'python'
+
---
+++ Python/P002_operators.py
@@ -0,0 +1,37 @@
+#Arithmetic Operator -> +,-,*,/,%
+
+a = 10
+# a = 1
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+radius = 5.6
+
+areaOfCircle = 3.14 * radius* radius
+print('area of circle ',areaOfCircle)
+
+
+#Assignment operator -> =,+=,-=,*=,/=
+a = 1
+
+a += 3 # a = a+3
+print(a)
+
+a -=2 # a = a-2
+print(a)
+
+a *= 2
+print(a)
+
+a/= 1
+print(a)
+
+b = 10
+
+a += b # a = a+b
+print(a)
