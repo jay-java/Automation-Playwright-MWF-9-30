a830af2c9b0257e3a8b38b912afed7e39b65059f
jay-java <jayprajapati.tops@gmail.com>
2026-07-15 10:34:29 +0530

    string

---
+++ Python/P006_string.py
@@ -0,0 +1,73 @@
+s = 'hello python 23457654$^&'
+print(s)
+s1 = "hello python"
+print(s1)
+
+s = 'Hello User "Python is best" to learn'
+print(s)
+
+s = 'Hello User \'Python is best\' to learn'
+print(s)
+
+s = '''Hello User
+Python is best to learn'''
+print(s)
+ 
+s= 'hello User Python is best to learn'
+print(s[6])
+print(s[-1])
+print(s[6:9])
+print(s[:5])
+print(s[10:])
+print(s[-10:])
+
+print(s[0:10:1])
+print(s[0:10:2])
+
+
+s1 = 'hello'
+s2 = 'hello'
+print(s1+' '+s2)
+s3 = s1 + s2
+print(s3)
+
+print(s1 * 3)
+print(3 * s1)
+
+print(s1 == s2)
+print(s1 != s2)
+print(s1 > s2)
+print(s1 >= s2)
+print(s1 < s2)
+print(s1 <= s2)
+
+
+s= 'hello User Python is best to learn'
+print('User' in s)
+print('User' not in s)
+
+
+name = '        UserName        ' 
+
+print(name)
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+new_name = name.strip()
+
+
+username = 'pythonLanguage besttolearn123'
+print(username.upper())
+print(username.lower())
+print(username.capitalize())
+print(username.isupper())
+print(username.islower())
+print(username.isalpha())
+print(username.isnumeric())
+print(username.isalnum())
+
+
+
+
+
+
